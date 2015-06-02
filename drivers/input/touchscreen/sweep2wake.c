44838af3f24ba16df83dfad1fce837ada76b07f0
Vineeth Raj <contact.twn@openmailbox.org>
2015-06-02 12:42:18 +0530

    s2w: align s2w variables

--- drivers/input/touchscreen/sweep2wake.c
+++ drivers/input/touchscreen/sweep2wake.c
@@ -57,10 +57,10 @@ MODULE_VERSION(DRIVER_VERSION);
 MODULE_LICENSE("GPLv2");
 
 /* Tuneables */
-#define S2W_DEBUG		1
-#define S2W_DEFAULT		1
-#define S2W_S2SONLY_DEFAULT	0
-#define S2W_PWRKEY_DUR          60
+#define S2W_DEBUG             1
+#define S2W_DEFAULT           1
+#define S2W_S2SONLY_DEFAULT   0
+#define S2W_PWRKEY_DUR       60
 
 #ifdef CONFIG_MACH_MSM8974_HAMMERHEAD
 /* Hammerhead aka Nexus 5 */
